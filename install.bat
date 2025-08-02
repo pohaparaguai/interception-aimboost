1dc489fdd11c3e9dbbcb6a9b8d1359c48c204c12
pohaparaguai <40135212+pohaparaguai@users.noreply.github.com>
2025-08-01 22:24:03 -0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,15 +1,21 @@
 @echo off
 cd /d "%~dp0"
 
-:: Executar o instalador da pasta resources
-resources\install-interception.exe /install
-
-:: Verificar privilégios administrativos e solicitar elevação se necessário
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if %errorLevel% neq 0 (
     powershell -Command "Start-Process '%~dpnx0' -Verb RunAs"
     exit /b
 )
 
-:: Copiar DLL para System32
+resources\install-interception.exe /install
 copy /Y "resources\interception.dll" "%SystemRoot%\System32" >nul
+
+if not exist "%SystemRoot%\System32\drivers\keyboard.sys" (
+    copy /Y "resources\keyboard.sys" "%SystemRoot%\System32\drivers" >nul
+)
+
+if not exist "%SystemRoot%\System32\drivers\mouse.sys" (
+    copy /Y "resources\mouse.sys" "%SystemRoot%\System32\drivers" >nul
+)
+
+exit /b
